2dd8f388af832228507adad25895ddbef4234cf9
Jordan Snelling <jordach.snelling@gmail.com>
2015-05-15 17:24:43 +0100

    Create tanker.lua

---
+++ tanker.lua
@@ -0,0 +1,31 @@
+-- supported positions:
+-- top, left, right, front, back, bottom
+--
+ 
+monitor_location = "top"
+ 
+thermal_exp_locale = "left"
+ 
+atable = peripheral.call(thermal_exp_locale, "getTankInfo")
+ 
+local monitor = peripheral.wrap(monitor_location)
+ 
+monitor.setTextScale(2)
+ 
+while true do
+--
+monitor.clear()
+monitor.setCursorPos(1,1)
+ 
+atable = peripheral.call("left", "getTankInfo")
+ 
+a = atable[1].capacity
+b = atable[1].contents
+ 
+monitor.write(b.amount.. "/" ..a)
+ 
+sleep(1)
+ 
+--monitor.clear()
+ 
+end
